0f978c998382fa55d7296f9ea62496e878d70673
Jaime Salgado <jsalg10@gmail.com>
2021-06-02 16:05:18 -0500

    completed palindrome

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,20 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
 function palindromeChecker(text) {
-    v// Code goes here
+  var reversedText = text.toLowerCase().split("").reverse().join("");
+
+  return text === reversedText;
 }
 
-
+function palindromeChecker(text) {
+  var textLen = text.length;
+  for (var i = 0; i < textLen / 2; i++) {
+    if (text[i] !== text[textLen - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = palindromeChecker;
