96f2d6f0ac95469179b1a4780ea6366bf8377031
Jaime Salgado <jsalg10@gmail.com>
2021-06-02 14:20:51 -0500

    finished lesson 5, counting vowels in string

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -2,12 +2,25 @@
 Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
-
-
+const Vowels = ["a", "e", "i", "o", "u"];
 function vowelsCounter(text) {
-    // Code goes here
+  let counter = 0;
+  for (let letter of text.toLowerCase()) {
+    if (Vowels.includes(letter)) {
+      counter++;
+    }
+  }
+  return counter;
 }
 
+function vowelsCounter(text) {
+  let matchingInstances = text.match(/[aeiou]/gi);
 
+  if (matchingInstances) {
+    return matchingInstances.length;
+  } else {
+    return 0;
+  }
+}
 
 module.exports = vowelsCounter;
