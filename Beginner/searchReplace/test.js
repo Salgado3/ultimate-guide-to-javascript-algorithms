61d850145058e2779494897d7da51e179c4c6a6d
Jaime Salgado <jsalg10@gmail.com>
2021-06-03 09:20:18 -0500

    working on test

--- Beginner/searchReplace/test.js
+++ Beginner/searchReplace/test.js
@@ -1,1 +1,17 @@
 const searchReplace = require("./index-START");
+
+test("searchReplace is a function", () => {
+  expect(typeof searchReplace).toEqual("function");
+});
+
+test("searchReplace a str of text", () => {
+  expect(searchReplace("str")).toEqual("string");
+});
+
+test("searchReplace a string containing numbs", () => {
+  expect(searchReplace("numbs")).toEqual("number");
+});
+
+test("searchReplace a string containing standard case characters", () => {
+  expect(searchReplace("standard")).toEqual("mixed");
+});
