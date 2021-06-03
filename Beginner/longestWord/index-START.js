eaa41935f45bc14fe7bf51677da3fd17b3026f4d
Jaime Salgado <jsalg10@gmail.com>
2021-06-02 22:10:22 -0500

    completed longest word

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -3,11 +3,37 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
 function longestWord(text) {
-    // Code goes here
+  let wordArray = text.split(" ");
+  let maxLength = 0;
+  let result = "";
+
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > maxLength) {
+      maxLength = wordArray[i].length;
+      result = wordArray[i];
+    }
+  }
+
+  return result;
 }
 
+function longestWord(text) {
+  var result = text.split(" ").reduce((maxLengthWord, currentWord) => {
+    if (currentWord.length > maxLengthWord.length) {
+      return currentWord;
+    } else {
+      return maxLengthWord;
+    }
+  }, "");
+  return result;
+}
 
-module.exports = longestWord
+function longestWord(text) {
+  var sortedArray = text
+    .split(" ")
+    .sort((wordA, wordB) => wordB.length - wordA.length);
+  return sortedArray[0];
+}
+
+module.exports = longestWord;
